1f5228dc7f8855df35df97b69dc4b32819b95c77
Husan <husanaulakh5@gmail.com>
2024-10-12 14:16:16 -0700

    useful notebooks

---
+++ finetuning_yolo.py
@@ -0,0 +1,9 @@
+from ultralytics import YOLO
+
+# Load a pretrained model
+model = YOLO(model='yolov8n.pt')
+
+# Train the model
+model.train(data="config.yaml", epochs=1, imgsz=640)
+metrics = model.val()
+path = model.export(format="onnx")  # export the model to ONNX format1~path = model.export(format="onnx")  # export the model to ONNX format
